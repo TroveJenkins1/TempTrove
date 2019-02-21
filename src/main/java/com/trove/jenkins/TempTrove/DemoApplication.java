2df100f79030dcb1929bbb22b534640a39cb94dd
Derek <djustinger@trovedata.com>
2019-02-21 14:35:26 -0500

    Initial Commit

---
+++ src/main/java/com/trove/jenkins/TempTrove/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.trove.jenkins.TempTrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
